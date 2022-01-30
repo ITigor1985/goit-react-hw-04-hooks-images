60991518d1c6ce819b924586fe5d0919b801f0f3
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-01-30 22:07:58 +0200

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -9,93 +9,84 @@ import './App.css';
 
 const items_on_the_page = 12;
 
-export function App (){
-  const [query, setQuery] = useState("");
+export function App() {
+  const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [currentHitsPerPage, setCurrentHitsPerPage] = useState(null);
   const [error, setError] = useState(null);
-  const [largeImageURL, setLargeImageURL]=useState("");
-  const [alt, setAlt]=useState("");
-  
-  
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [alt, setAlt] = useState('');
 
-  useEffect(() =>{
+  useEffect(() => {
+    async function getImagesData() {
+      try {
+        setIsLoading(true);
+        const { hits, totalHits } = await getImages(query, page);
+        if (totalHits === 0) {
+          alert('Nothing found');
+          setIsLoading(false);
+          setCurrentHitsPerPage(null);
+          return;
+        }
+        const images = hits.map(({ id, largeImageURL, tags, webformatURL }) => {
+          return { id, largeImageURL, tags, webformatURL };
+        });
+
+        setImages(prevState => {
+          return [...prevState, ...images];
+        });
+        if (page > 1) {
+          window.scrollTo({
+            top: document.documentElement.scrollHeight,
+            behavior: 'smooth',
+          });
+        }
+        setCurrentHitsPerPage(hits.length);
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
     if (!query) {
       return;
     }
     getImagesData();
-    if(page > 1){
-      window.scrollTo({
-        top: document.documentElement.scrollHeight,
-        behavior: 'smooth',
-      });
-    }
-    
-  },[query, page])
+  }, [query, page]);
 
-  useEffect(() => {window.addEventListener('keydown', cleanEventListener);})
+  useEffect(() => {
+    window.addEventListener('keydown', cleanEventListener);
+  });
 
-  const handleFormSubmit = (query) => {
+  const handleFormSubmit = query => {
     setQuery(query);
     setPage(1);
     setImages([]);
   };
 
-  async function getImagesData() {
-    
-    try {
-      setIsLoading(true);
-      const { hits, totalHits } = await getImages(query, page);
-      if (totalHits === 0) {
-        alert('Nothing found');
-        setIsLoading(false);
-        setCurrentHitsPerPage(null);
-        return;
-      }
-      const images = makeImagesArray(hits);
-
-      setImages((prevState) => {
-        return [...prevState, ...images];
-      });
-      setCurrentHitsPerPage(hits.length);
-          
-
-    } catch (error) {
-      console.log(error);
-      setError(error.message );
-    } finally {
-      setIsLoading(false);
-    }
-  }
-  const makeImagesArray = data => {
-    return data.map(({ id, largeImageURL, tags, webformatURL }) => {
-      return { id, largeImageURL, tags, webformatURL };
-    });
-  };
-
   const handleLoadMoreClick = () => {
-    setPage(page+1);     
-    console.log(page)
+    setPage(page + 1);
   };
 
- const modalOpen = (moduleUrl, moduleAlt, event) => {
+  const modalOpen = (moduleUrl, moduleAlt, event) => {
     event.preventDefault();
     setLargeImageURL(moduleUrl);
-    setAlt(moduleAlt);    
+    setAlt(moduleAlt);
   };
 
   const modalClose = () => {
     setLargeImageURL('');
-    setAlt(''); 
-      };
-    
-    const  cleanEventListener = e => {
-        if (e.code === 'Escape') {
-          modalClose();
-        }
-      };
+    setAlt('');
+  };
+
+  const cleanEventListener = e => {
+    if (e.code === 'Escape') {
+      modalClose();
+    }
+  };
   return (
     <div>
       <Searchbar onSubmit={handleFormSubmit} />
@@ -114,22 +105,14 @@ export function App (){
       {isLoading && <Loader />}
       {error && <h2 className="Message">Please try again</h2>}
       {largeImageURL && (
-        <Modal
-          largeImageURL={largeImageURL}
-          alt={alt}
-          onClick={modalClose}
-        />
+        <Modal largeImageURL={largeImageURL} alt={alt} onClick={modalClose} />
       )}
     </div>
   );
-
 }
-
 
 // export class App extends Component {
 
-
-  
 //   state = {
 //     isLoading: false,
 //     images: [],
@@ -160,39 +143,39 @@ export function App (){
 //     }
 
 //     if (this.state.page > 2) {
-      // window.scrollTo({
-      //   top: document.documentElement.scrollHeight,
-      //   behavior: 'smooth',
-      // });
+// window.scrollTo({
+//   top: document.documentElement.scrollHeight,
+//   behavior: 'smooth',
+// });
 //     }
 //   }
 
-  // async getImagesData() {
-  //   const { query, page } = this.state;
-  //   try {
-  //     this.setState({ isLoading: true });
-  //     const { hits, totalHits } = await getImages(query, page);
-  //     if (totalHits === 0) {
-  //       alert('Nothing found');
-  //       this.setState({ isLoading: false, currentHitsPerPage: null });
-  //       return;
-  //     }
-  //     const images = this.makeImagesArray(hits);
+// async getImagesData() {
+//   const { query, page } = this.state;
+//   try {
+//     this.setState({ isLoading: true });
+//     const { hits, totalHits } = await getImages(query, page);
+//     if (totalHits === 0) {
+//       alert('Nothing found');
+//       this.setState({ isLoading: false, currentHitsPerPage: null });
+//       return;
+//     }
+//     const images = this.makeImagesArray(hits);
 
-  //     this.setState(prevState => {
-  //       return {
-  //         images: [...prevState.images, ...images],
-  //         currentHitsPerPage: hits.length,
-  //         page: prevState.page + 1,
-  //       };
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //     this.setState({ error: error.message });
-  //   } finally {
-  //     this.setState({ isLoading: false });
-  //   }
-  // }
+//     this.setState(prevState => {
+//       return {
+//         images: [...prevState.images, ...images],
+//         currentHitsPerPage: hits.length,
+//         page: prevState.page + 1,
+//       };
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     this.setState({ error: error.message });
+//   } finally {
+//     this.setState({ isLoading: false });
+//   }
+// }
 
 //   makeImagesArray = data => {
 //     return data.map(({ id, largeImageURL, tags, webformatURL }) => {
@@ -200,56 +183,55 @@ export function App (){
 //     });
 //   };
 
-  // handleLoadMoreClick = () => {
-  //   this.getImagesData();
-  // };
+// handleLoadMoreClick = () => {
+//   this.getImagesData();
+// };
 
-  // modalOpen = (moduleUrl, moduleAlt, event) => {
-  //   event.preventDefault();
-  //   this.setState({
-  //     largeImageURL: moduleUrl,
-  //     alt: moduleAlt,
-  //   });
-  // };
-  
+// modalOpen = (moduleUrl, moduleAlt, event) => {
+//   event.preventDefault();
+//   this.setState({
+//     largeImageURL: moduleUrl,
+//     alt: moduleAlt,
+//   });
+// };
 
 //   modalClose = () => {
 //     this.setState({ largeImageURL: '', alt: '' });
 //   };
 
-  // cleanEventListener = e => {
-  //   if (e.code === 'Escape') {
-  //     this.modalClose();
-  //   }
-  // };
+// cleanEventListener = e => {
+//   if (e.code === 'Escape') {
+//     this.modalClose();
+//   }
+// };
 
 //   render() {
 //     const { images, currentHitsPerPage, error, isLoading } = this.state;
-    // return (
-    //   <div>
-    //     <Searchbar onSubmit={this.handleFormSubmit} />
+// return (
+//   <div>
+//     <Searchbar onSubmit={this.handleFormSubmit} />
 
-    //     {images.length > 0 && !error && (
-    //       <>
-    //         <ImageGallery images={images} modalOpen={this.modalOpen} />
-    //         {currentHitsPerPage && currentHitsPerPage < items_on_the_page && (
-    //           <p className="Message">End of search results</p>
-    //         )}
-    //       </>
-    //     )}
-    //     {currentHitsPerPage === items_on_the_page && !isLoading && (
-    //       <Button onClick={this.handleLoadMoreClick} />
-    //     )}
-    //     {isLoading && <Loader />}
-    //     {error && <h2 className="Message">Please try again</h2>}
-    //     {this.state.largeImageURL && (
-    //       <Modal
-    //         largeImageURL={this.state.largeImageURL}
-    //         alt={this.state.alt}
-    //         onClick={this.modalClose}
-    //       />
-    //     )}
-    //   </div>
-    // );
+//     {images.length > 0 && !error && (
+//       <>
+//         <ImageGallery images={images} modalOpen={this.modalOpen} />
+//         {currentHitsPerPage && currentHitsPerPage < items_on_the_page && (
+//           <p className="Message">End of search results</p>
+//         )}
+//       </>
+//     )}
+//     {currentHitsPerPage === items_on_the_page && !isLoading && (
+//       <Button onClick={this.handleLoadMoreClick} />
+//     )}
+//     {isLoading && <Loader />}
+//     {error && <h2 className="Message">Please try again</h2>}
+//     {this.state.largeImageURL && (
+//       <Modal
+//         largeImageURL={this.state.largeImageURL}
+//         alt={this.state.alt}
+//         onClick={this.modalClose}
+//       />
+//     )}
+//   </div>
+// );
 //   }
 // }
